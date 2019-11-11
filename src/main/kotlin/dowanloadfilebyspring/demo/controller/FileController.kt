b347592e5100bb6b731a9829a6c82bd722bffd9a
ZhenyaSUPREME <evgeniy_goncharov_98@mail.ru>
2019-11-11 21:10:11 +0300

    Upload and Download file

--- src/main/kotlin/dowanloadfilebyspring/demo/controller/FileController.kt
+++ src/main/kotlin/dowanloadfilebyspring/demo/controller/FileController.kt
@@ -1,13 +1,21 @@
 package dowanloadfilebyspring.demo.controller
 
 import dowanloadfilebyspring.demo.servis.ServisUpload
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.http.codec.multipart.FilePart
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.server.reactive.ServerHttpResponse
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 import java.awt.PageAttributes
+import org.synchronoss.cloud.nio.multipart.MultipartUtils.getHeaders
+import org.springframework.http.ZeroCopyHttpOutputMessage
+import java.io.File
+import java.io.IOException
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.GetMapping
+import java.nio.file.Paths
+
 
 @RestController
 class FileController(val servisUpload:ServisUpload) {
@@ -16,4 +24,21 @@ class FileController(val servisUpload:ServisUpload) {
     {
         return servisUpload.save(filePart)
     }
+//    @GetMapping("/download/{name:.+}")
+//    public fun downloadByWriteWith(response: ServerHttpResponse,@PathVariable name:String):Mono<Void>{
+//        val zeroCopyResponse = response as ZeroCopyHttpOutputMessage
+//        response.getHeaders().set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=$name")
+//        response.getHeaders().contentType = MediaType.APPLICATION_OCTET_STREAM
+//       var file=servisUpload.download(name)
+//        return zeroCopyResponse.writeWith(file, 0, file.length());
+//    }
+@GetMapping("/download/{fileName:.+}")
+@Throws(IOException::class)
+fun downloadByWriteWith(@PathVariable fileName: String, response: ServerHttpResponse): Mono<Void> {
+    val zeroCopyResponse = response as ZeroCopyHttpOutputMessage
+    response.getHeaders().set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=uploads/$fileName")
+    val file = Paths.get("upload/$fileName").toFile()
+    response.getHeaders().contentType = MediaType.parseMediaType("application/octet-stream")
+    return zeroCopyResponse.writeWith(file, 0, file.length())
+}
 }
--- src/main/kotlin/dowanloadfilebyspring/demo/servis/ServisUpload.kt
+++ src/main/kotlin/dowanloadfilebyspring/demo/servis/ServisUpload.kt
@@ -4,26 +4,34 @@ import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.http.codec.multipart.FilePart
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
+import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardOpenOption
 import java.nio.channels.AsynchronousFileChannel
 import java.nio.file.Files
+import org.springframework.core.io.ClassPathResource
+
+
 
 
 @Service
 class ServisUpload {
-    val base:Path = Paths.get("/upload")
-    //в функцию передаётся имя,а выводится абсолютный путь.
-    private fun getPath(name:String)= base.resolve(name)
-    fun save(filePart: FilePart):Mono<String>{
+
+    fun save(filePart: FilePart): Mono<String> {
         val path = Files.createFile(Paths.get("upload", filePart.filename()))
         val channel = AsynchronousFileChannel.open(path, StandardOpenOption.WRITE)
         //databuffer специализированный класс,который создаёт ,закрывает,обслуживает буффер,в которому подключаются каналы
         DataBufferUtils.write(filePart.content(), channel, 0)
                 .doOnComplete { println("finish") }
                 .subscribe()
-                return Mono.just("create")
+        return Mono.just("create")
     }
 
+    fun download(name: String): File {
+        // val resource = ClassPathResource(name)
+        // val file = resource.file
+        var file = Paths.get("/upload/$name").toFile()
+        return file
+    }
 }
